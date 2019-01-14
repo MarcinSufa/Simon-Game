17b36a00d9cd186f0ebd3b8848c08e9f2bdef422
MarcinSufa <45658626+MarcinSufa@users.noreply.github.com>
2019-01-15 00:00:21 +0100

    Update game.js
    Fixed bug with many kayboard clicks which increased level of the game

--- game.js
+++ game.js
@@ -1,4 +1,3 @@
-
 var buttonColours = ["red", "blue", "green", "yellow"];
 
 var gamePattern = [];
@@ -10,39 +9,43 @@ var levelNumber = 0;
 // Start the game when a key is pressed
 
 $(document).keypress(function(startGame) {
-  nextSequence();
+  if (levelNumber==0){
+  nextSequence();}
+  else {
+    alert("The Game has starded. Click on the button");
+  }
 });
 
 
 
+
 // check if answer is correct, after button clicked
-function checkAnswer (){
+function checkAnswer() {
 
-  for (var i =0; i<userClickedPattern.length; i++ ) {
-  var n = userClickedPattern[i];
-    if (gamePattern[i]==userClickedPattern[i]){
-      console.log("number "+i+" is true");
+  for (var i = 0; i < userClickedPattern.length; i++) {
+    var n = userClickedPattern[i];
+    if (gamePattern[i] == userClickedPattern[i]) {
+      console.log("number " + i + " is true");
 
 
-    }
-  else {
+    } else {
       endGame();
     }
 
- }
- if (gamePattern.length==userClickedPattern.length&&levelNumber!=0){
-   console.log("wtf");
-   userClickedPattern = [];
-   setTimeout( nextSequence,1000 );}
- else {
-  console.log("else end of checkAnswer");
- }
+  }
+  if (gamePattern.length == userClickedPattern.length && levelNumber != 0) {
+    console.log("wtf");
+    userClickedPattern = [];
+    setTimeout(nextSequence, 1000);
+  } else {
+    console.log("else end of checkAnswer");
+  }
 
 }
 
 
 // reset the game when user clicked on wrong button + make css animation and audio
-function endGame () {
+function endGame() {
   gamePattern = [];
   userClickedPattern = [];
   levelNumber = 0;
@@ -51,9 +54,9 @@ function endGame () {
   // wrong answer stylesheet
   document.getElementsByTagName("BODY")[0].classList.add("game-over");
 
-  setTimeout(function (){
-  document.getElementsByTagName("BODY")[0].classList.remove("game-over");
-  }, 100 );
+  setTimeout(function() {
+    document.getElementsByTagName("BODY")[0].classList.remove("game-over");
+  }, 100);
   var wrongSound = new Audio('sounds/wrong.mp3');
   wrongSound.play();
 
@@ -64,7 +67,7 @@ function endGame () {
 
 // Checking which color was clicked by user + css animation and audio
 
- document.getElementById("red").onclick = function red(){
+document.getElementById("red").onclick = function red() {
   userClickedPattern.push("red");
   var redSound = new Audio('sounds/red.mp3');
   redSound.play();
@@ -73,51 +76,51 @@ function endGame () {
 
   document.getElementById("red").classList.add("pressed");
 
-  setTimeout(function time1(){
-  document.getElementById("red").classList.remove("pressed");
-  }, 100 );
+  setTimeout(function time1() {
+    document.getElementById("red").classList.remove("pressed");
+  }, 100);
   checkAnswer();
 };
 
 
 
- document.getElementById("blue").onclick = function red() {
-    userClickedPattern.push("blue");
-    var blueSound = new Audio('sounds/blue.mp3');
-    blueSound.play();
+document.getElementById("blue").onclick = function red() {
+  userClickedPattern.push("blue");
+  var blueSound = new Audio('sounds/blue.mp3');
+  blueSound.play();
 
-    document.getElementById("blue").classList.add("pressed");
+  document.getElementById("blue").classList.add("pressed");
 
-    setTimeout(function time2(){
+  setTimeout(function time2() {
     document.getElementById("blue").classList.remove("pressed");
-    }, 100 );
-    checkAnswer();
+  }, 100);
+  checkAnswer();
 
 
 };
-  document.getElementById("green").onclick = function green() {
+document.getElementById("green").onclick = function green() {
   userClickedPattern.push("green");
   var greenSound = new Audio('sounds/green.mp3');
   greenSound.play();
 
   document.getElementById("green").classList.add("pressed");
 
-  setTimeout(function time3(){
-  document.getElementById("green").classList.remove("pressed");
-  }, 100 );
+  setTimeout(function time3() {
+    document.getElementById("green").classList.remove("pressed");
+  }, 100);
   checkAnswer();
 
 };
-  document.getElementById("yellow").onclick = function yellow() {
+document.getElementById("yellow").onclick = function yellow() {
   userClickedPattern.push("yellow");
   var yellowSound = new Audio('sounds/yellow.mp3');
   yellowSound.play();
 
   document.getElementById("yellow").classList.add("pressed");
 
-  setTimeout(function time4(){
-  document.getElementById("yellow").classList.remove("pressed");
-  }, 100 );
+  setTimeout(function time4() {
+    document.getElementById("yellow").classList.remove("pressed");
+  }, 100);
   checkAnswer();
 };
 
@@ -127,48 +130,48 @@ function endGame () {
 // generating rando number and select color from buttonColours to add in gamePattern, after that adding +1 to level and animate the button selected
 
 function nextSequence(randomNumber, randomChosenColour) {
- randomNumber = Math.floor(Math.random()*4);
+  randomNumber = Math.floor(Math.random() * 4);
   console.log(randomNumber);
-randomChosenColour = buttonColours[randomNumber];
+  randomChosenColour = buttonColours[randomNumber];
 
-gamePattern.push(randomChosenColour);
+  gamePattern.push(randomChosenColour);
 
- var newSound = new Audio('sounds/'+randomChosenColour+'.mp3');
- newSound.play();
+  var newSound = new Audio('sounds/' + randomChosenColour + '.mp3');
+  newSound.play();
 
-// dodajemy numer do levela w h1
-levelNumber ++;
- $("#level-title").text("Level "+levelNumber);
+  // dodajemy numer do levela w h1
+  levelNumber++;
+  $("#level-title").text("Level " + levelNumber);
 
-console.log(randomChosenColour);
-console.log(gamePattern);
-buttonAnimation();
+  console.log(randomChosenColour);
+  console.log(gamePattern);
+  buttonAnimation();
 }
 
 // animation of buttons by adding and removing css class pressed
 
 function removeButtonAnimation() {
-  var activeButton = document.getElementById(gamePattern[gamePattern.length-1]);
- activeButton.classList.remove("pressed");
+  var activeButton = document.getElementById(gamePattern[gamePattern.length - 1]);
+  activeButton.classList.remove("pressed");
 }
 
 function removeButtonAnimation2() {
-  var activeButton2 = document.getElementById(userClickedPattern[userClickedPattern.length-1]);
- activeButton2.classList.remove("pressed");
+  var activeButton2 = document.getElementById(userClickedPattern[userClickedPattern.length - 1]);
+  activeButton2.classList.remove("pressed");
 }
 
 function buttonAnimation() {
-    var activeButton = document.getElementById(gamePattern[gamePattern.length-1]);
-    activeButton.classList.add("pressed");
+  var activeButton = document.getElementById(gamePattern[gamePattern.length - 1]);
+  activeButton.classList.add("pressed");
 
-    setTimeout(removeButtonAnimation, 100 );
+  setTimeout(removeButtonAnimation, 100);
 
 }
 
 
 function buttonAnimationClick() {
-    var activeButton2 = document.getElementById(userClickedPattern[userClickedPattern.length-1]);
-    activeButton2.classList.add("pressed");
+  var activeButton2 = document.getElementById(userClickedPattern[userClickedPattern.length - 1]);
+  activeButton2.classList.add("pressed");
 
-      setTimeout(removeButtonAnimation2, 100 );
+  setTimeout(removeButtonAnimation2, 100);
 }
